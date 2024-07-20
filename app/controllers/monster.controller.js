3ec4fc1ef5ee58cfe74cce9928c1834fe4707366
Alain Bonneau <alaindev97@gmail.com>
2024-07-20 21:55:33 +0200

    add commentary

--- app/controllers/monster.controller.js
+++ app/controllers/monster.controller.js
@@ -1,10 +1,12 @@
 import { Monster } from "../models/monster.model.js";
 
 const monsterController = {
+  // Permet de récupérer tous les monstres.
   getAllMonster: async (req, res) => {
     const monsterList = await Monster.findAll();
     res.json(monsterList);
   },
+  // Permet de récupérer un monstre via son id.
   getMonsterById: async (req, res) => {
     const monster = await Monster.findByPk(req.params.id);
     res.json(monster);
