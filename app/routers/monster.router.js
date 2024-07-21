f8b08855033da47163e99bf994c48b875df9d958
Alain Bonneau <alaindev97@gmail.com>
2024-07-21 23:37:20 +0200

    add updateMonster

--- app/routers/monster.router.js
+++ app/routers/monster.router.js
@@ -5,3 +5,4 @@ export const router = Router();
 
 router.get("/monstre", monsterController.getAllMonster);
 router.get("/monstre/:id", monsterController.getMonsterById);
+router.post("/monstre/:id", monsterController.updateMonster);
