8981cb0a86ec09b582b89d1f4a15efd6aea97af9
Alain Bonneau <alaindev97@gmail.com>
2024-07-21 21:11:04 +0200

    WIP: add getmonsterbyid

--- app/routers/monster.router.js
+++ app/routers/monster.router.js
@@ -4,3 +4,4 @@ import monsterController from "../controllers/monster.controller.js";
 export const router = Router();
 
 router.get("/monstre", monsterController.getAllMonster);
+router.get("/monstre/:id", monsterController.getMonsterById);
