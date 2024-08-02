e64f4bbd9b4ef01933479c7a72dbb37dfb1f8bbf
Alain Bonneau <alaindev97@gmail.com>
2024-08-02 02:14:32 +0200

    add security

--- app/controllers/resistance.controller.js
+++ app/controllers/resistance.controller.js
@@ -1,13 +1,21 @@
 import { Resistance } from "../models/resistance.model.js";
 
 const resistanceController = {
+  // Permet de récupérer toutes les résistances.
   getAllResistance: async (req, res) => {
     const resistanceList = await Resistance.findAll();
+    if (!resistanceList) {
+      return res.status(404).json({ error: "Aucune résistance trouvée" });
+    }
     res.json(resistanceList);
   },
 
+  // Permet de récupérer une résistance via son id.
   GetOneResistance: async (req, res) => {
     const resistance = await Resistance.findByPk(req.params.id);
+    if (!resistance) {
+      return res.status(404).json({ error: "Résistance inconnue" });
+    }
     res.json(resistance);
   },
 };
--- app/routers/router.js
+++ app/routers/router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { router as monsterRouter } from "./monster.router.js";
 import { router as breedRouter } from "./breed.router.js";
+import { router as resistanceRouter } from "./resistance.router.js";
 
 export const router = Router();
 
 router.use(monsterRouter);
 router.use(breedRouter);
+router.use(resistanceRouter);
